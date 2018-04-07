0304f9973952b5a8ae85b08d1446d2ccd995c4e5
ZT <125107064@qq.com>
2018-04-07 22:16:35 +0800

    test

---
+++ js/baidu.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	/*点击按钮给a标签修改属性值*/
+	$("a.nav_box").click(function(){
+		$("a.nav_box").attr("id","");
+		$(this).attr("id","select");
+	});
+	
+	/*克隆添加html*/
+	
+});
