5d9d02629c0bd41606d9b983d14b9af8a01f6698
ZT <125107064@qq.com>
2018-04-08 10:13:00 +0800

    test

--- js/baidu.js
+++ js/baidu.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
+	/*克隆添加html*/
+	function scrillside(){
+		var box=$(window).height();
+		console.log(box);
+	}
+	
 	/*点击按钮给a标签修改属性值*/
 	$("a.nav_box").click(function(){
 		$("a.nav_box").attr("id","");
 		$(this).attr("id","select");
 	});
 	
-	/*克隆添加html*/
-	
+
 });
