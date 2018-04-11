e08743ad5634db9f8858666d9166d58ed52ba2a7
ZT <125107064@qq.com>
2018-04-11 17:31:00 +0800

    test

--- js/baidu.js
+++ js/baidu.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+		/*点击打开后台切换页面*/
+	$("#logo_open").click(function(){
+		$(".middle_box").find("iframe").attr("src","/server/baidu_houtai.html");
+	});
+	
 	/*克隆添加html*/
 	newBOx();
-		
 
 	/*点击按钮给a标签修改属性值*/
 	$("a.nav_box").click(function(){
@@ -10,7 +14,7 @@ $(document).ready(function(){
 		
 		/*添加ajax请求*/
 		$.ajax({
-			type:"POST",
+			type:"GET",
 			url:"../server/baidu.py",
 			dataType:"json",
 			data:{
@@ -19,14 +23,12 @@ $(document).ready(function(){
 			async:true
 		});
 	});
-	
-
 });
 
 	function newBOx(){
 		var $lists=$(".middle_box");
 		/*先清空，再加载 */
-		//$($lists).empty();
+		$($lists).empty();
 		var $list=$("<div></div>").addClass("new_box").prependTo($lists);
 		var $title=$("<div></div>").addClass("new_title").appendTo($list);
 		var $pic=$("<div></div>").addClass("new_pic").appendTo($list);
